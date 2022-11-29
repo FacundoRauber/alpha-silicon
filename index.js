0c93eed24f8fb524a5499aa0fc1338b851ca1cd4
FacundoRauber <dis.jacku@gmail.com>
2022-11-28 23:08:21 -0300

    updated code for deploy personas

---
+++ index.js
@@ -0,0 +1,32 @@
+require("rootpath")();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const config = require("./src/config/config.json");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+morgan(':method :url :status :res[content-length] - :response-time ms');
+  
+
+
+app.get("/", function (req, res) {
+    res.send("ALPHA-SILICON");
+});
+
+const personaCont = require("./src/controller/personaController.js");
+app.use("/api/persona",personaCont);
+
+/*
+const userCont = require("userController.js");
+app.use("/api/usuario",userCont);
+*/
+
+app.listen(config.server.port, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server iniciado en puerto:${config.server.port}`);
+    }
+});
